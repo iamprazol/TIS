b28b60024d0c362d218491118db49a2a72094a55
Sujan Pandey <sazeal.sujan@gmail.com>
2019-05-19 08:39:14 +0545

    Added logo and changed footer

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,8 +1,7 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="#" class="font-weight-bold ml-1" target="_blank">Prajjwal Poudel</a> &amp;
-            <a href="#" class="font-weight-bold ml-1" target="_blank">Sujan Pandey</a>
+            &copy; {{ now()->year }} <a href="" class="font-weight-bold ml-1" target="_blank">Infomax Pvt. Ltd
         </div>
     </div>
     <div class="col-xl-6">
