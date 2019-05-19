f22429ddd932489eee0b6b46b7475e1995670a94
Sujan Pandey <sazeal.sujan@gmail.com>
2019-05-19 09:17:23 +0545

    Logo

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,14 +1,7 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="" class="font-weight-bold ml-1" target="_blank">Infomax Pvt. Ltd
+            &copy; {{ now()->year }} <a href="https://www.infomax.com.np" class="font-weight-bold ml-1" target="_blank">Infomax Pvt. Ltd
         </div>
-    </div>
-    <div class="col-xl-6">
-        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-            <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-            </li>
-        </ul>
     </div>
 </div>
